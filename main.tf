6e12f8cbdd27e35ee8d364a7d4df54f97e32d64b
santosh <skm123_2000@yahoo.com>
2021-02-10 19:21:59 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -124,7 +124,8 @@ resource "aws_instance" "hashicat" {
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
   tags = {
-    Name = "devops"
+    Department = "devops"
+    Billable = "true"
   }
 }
 
